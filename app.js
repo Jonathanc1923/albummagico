11bb659cc4a1125e1e922f660224d7c9bcb1435d
Jonathanc1923 <jonathanencina2020@gmail.com>
2025-01-23 15:58:35 -0500

    22

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // Inicializa EmailJS
-emailjs.init("6-F1FsinyTjkCvfiT");
- // Reemplaza con tu User ID
+emailjs.init("6-F1FsinyTjkCvfiT"); // Reemplaza con tu User ID
 
 // Captura el evento de envío del formulario
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Evita que el formulario recargue la página
+  event.preventDefault(); // Previene la recarga de la página al enviar el formulario
 
   // Envía los datos del formulario a EmailJS
-  emailjs.sendForm("YOservice_yezgxgd", "template_0d3t4mr", this)
-    .then(function() {
-      alert("Correo enviado correctamente.");
+  emailjs.sendForm("service_yezgxgd", "template_0d3t4mr", this)
+    .then(function(response) {
+      alert("Correo enviado correctamente: " + response.status);
     }, function(error) {
       alert("Error al enviar el correo: " + JSON.stringify(error));
     });
